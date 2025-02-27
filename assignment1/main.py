a52c5eacbd63cdb5b9a698050cbfe9486390b660
Gabriele Bettineschi <gbettineschi29@gmail.com>
2025-02-27 15:35:41 +0100

    lint assignment 1

--- assignment1/main.py
+++ assignment1/main.py
@@ -1,4 +1,4 @@
-from pulp import *
+import pulp as pl
 
 
 def ex1():
@@ -8,9 +8,9 @@ def ex1():
     retval["obj"] = None
     retval["tight_constraints"] = [None]
 
-    problem = LpProblem("ex1", LpMinimize)
-    x = LpVariable("x")
-    y = LpVariable("y")
+    problem = pl.LpProblem("ex1", pl.LpMinimize)
+    x = pl.LpVariable("x")
+    y = pl.LpVariable("y")
 
     problem += x >= -10
     problem += y <= 10
@@ -20,11 +20,11 @@ def ex1():
     problem += 5 * x - 6 * y >= -100
 
     problem += 122 * x + 143 * y
-    problem.solve(PULP_CBC_CMD(msg=False))
+    problem.solve(pl.PULP_CBC_CMD(msg=False))
 
-    retval["x"] = value(x)
-    retval["y"] = value(y)
-    retval["obj"] = value(problem.objective)
+    retval["x"] = pl.value(x)
+    retval["y"] = pl.value(y)
+    retval["obj"] = pl.value(problem.objective)
     retval["tight_constraints"] = [
         i
         for i, constraint in enumerate(problem.constraints.values(), start=1)
@@ -58,23 +58,23 @@ def ex2():
         else:
             return -1
 
-    problem = LpProblem("ex2", LpMaximize)
-    strategy = LpVariable.dicts("x", choices, lowBound=0)
-    obj = LpVariable("obj")
+    problem = pl.LpProblem("ex2", pl.LpMaximize)
+    strategy = pl.LpVariable.dicts("x", choices, lowBound=0)
+    obj = pl.LpVariable("obj")
 
     # constrain probabilites to sum to 1
-    problem += lpSum(strategy.values()) == 1
+    problem += pl.lpSum(strategy.values()) == 1
 
     # constrain obj to be the minimum payoff of strategy, i.e. the payoff assuming a rational competitor
     for j in choices:
-        problem += obj <= lpSum(strategy[i] * payoff(i, j) for i in choices)
+        problem += obj <= pl.lpSum(strategy[i] * payoff(i, j) for i in choices)
 
     problem += obj
-    problem.solve(PULP_CBC_CMD(msg=False))
+    problem.solve(pl.PULP_CBC_CMD(msg=False))
 
     for k, v in strategy.items():
-        retval[f"x{k}"] = value(v)
-    retval["obj"] = value(obj)
+        retval[f"x{k}"] = pl.value(v)
+    retval["obj"] = pl.value(obj)
 
     return retval
 
@@ -93,18 +93,18 @@ def ex3():
                 contracts.append((i, j))
 
     # initialize LP relaxation of the integer problem assuming an integral solution
-    problem = LpProblem("ex3", LpMinimize)
-    delegations = LpVariable.dicts("x", companies, lowBound=0)
+    problem = pl.LpProblem("ex3", pl.LpMinimize)
+    delegations = pl.LpVariable.dicts("x", companies, lowBound=0)
 
     for i, j in contracts:
         problem += delegations[i] + delegations[j] >= 2
 
-    problem += lpSum(delegations.values())
-    problem.solve(PULP_CBC_CMD(msg=False))
+    problem += pl.lpSum(delegations.values())
+    problem.solve(pl.PULP_CBC_CMD(msg=False))
 
     for i in companies:
-        retval[f"x{i}"] = value(delegations[i])
-    retval["obj"] = value(problem.objective)
+        retval[f"x{i}"] = pl.value(delegations[i])
+    retval["obj"] = pl.value(problem.objective)
 
     return retval
 
