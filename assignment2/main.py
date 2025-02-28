560303979cd85940fc9cddd3291f638962b68be5
Gabriele Bettineschi <gbettineschi29@gmail.com>
2025-02-28 10:16:27 +0100

    fix assignment2

--- assignment2/main.py
+++ assignment2/main.py
@@ -6,15 +6,15 @@ def get_normalized_images(file_names):
     """Load images as numpy arrays from text files and normalize them."""
 
     images = {}
-    image_shape = None
+    expected_shape = None
 
     for file_name in file_names:
         with open(file_name, "r") as f:
             lines = [line.strip() for line in f.readlines() if line.strip()]
             array = np.array([[int(i) for i in line] for line in lines])
-            if image_shape is None:
-                image_shape = array.shape
-            elif image_shape != array.shape:
+            if expected_shape is None:
+                expected_shape = array.shape
+            elif expected_shape != array.shape:
                 raise ValueError(
                     f"""Shape of {file_name} is different
                      from the shape of previously loaded files."""
@@ -28,24 +28,32 @@ def get_normalized_images(file_names):
     return normalized_images
 
 
-def comp_dist(image1, image2):
-    """Returns the Earth Mover's Distance between two images."""
+def comp_dist(image1, image2, direction=1):
+    """
+    Returns the Earth Mover's Distance between two images.
+    The two images should:
+        - Have the same shape.
+        - Have the same positive sum of pixel values.
+    """
 
-    def point_dist(p1, p2):
+    def point_dist(p1, p2, direction):
+        """
+        Returns the horizontal distance between p1 and p2 allowing
+        strictly monotonous movement and one wrap-around.
+        """
         lengthX = image1.shape[1]
-        return (
-            p2[1] - p1[1]
-        ) % lengthX  # Wrap around in a single (clockwise) direction
+        raw_distance = (p2[1] - p1[1]) * direction
+        return raw_distance if raw_distance > 0 else lengthX + raw_distance
 
     if np.array_equal(image1, image2):
         return 0.0
 
-    non_zero_points1 = np.argwhere(image1)
-    non_zero_points2 = np.argwhere(image2)
-    nodes1 = {(1, *p): {"demand": float(-image1[tuple(p)])} for p in non_zero_points1}
-    nodes2 = {(2, *p): {"demand": float(image2[tuple(p)])} for p in non_zero_points2}
+    non_zero_points1 = [tuple(p) for p in np.argwhere(image1)]
+    non_zero_points2 = [tuple(p) for p in np.argwhere(image2)]
+    nodes1 = {(1, *p): {"demand": -image1[p]} for p in non_zero_points1}
+    nodes2 = {(2, *p): {"demand": image2[p]} for p in non_zero_points2}
     edges = [
-        ((1, *p1), (2, *p2), {"weight": point_dist(p1, p2)})
+        ((1, *p1), (2, *p2), {"weight": point_dist(p1, p2, direction)})
         for p1 in non_zero_points1
         for p2 in non_zero_points2
     ]
@@ -59,9 +67,8 @@ def comp_dist(image1, image2):
     return float(emd_distance)
 
 
-# This function should sort the files as described in the instructions.
-# P1.txt should be the first one.
 def sort_files():
+    """Sorts the files as described in the instructions."""
     files = [
         "P1.txt",
         "P2.txt",
@@ -80,8 +87,12 @@ def sort_files():
         "P15.txt",
     ]
     start_file = "P1.txt"
+    direction = 1
+
     images = get_normalized_images(files)
-    sorted_files = sorted(files, key=lambda x: comp_dist(images[start_file], images[x]))
+    sorted_files = sorted(
+        files, key=lambda x: comp_dist(images[start_file], images[x], direction)
+    )
     return sorted_files
 
 
